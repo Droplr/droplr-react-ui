cb27757ff2066991972ae700dfac0903d404bd91
Rafal Swietek <raf.swietek@gmail.com>
2019-01-07 17:31:41 +0100

    Build with new icons set

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,15 +1,21 @@
 import AddPeopleIcon from '../../svg/addPeople.svg';
 import AddToBoardIcon from '../../svg/addToBoard.svg';
+import AlignCenterIcon from '../../svg/alignCenter.svg';
+import AlignLeftIcon from '../../svg/alignLeft.svg';
+import AlignRightIcon from '../../svg/alignRight.svg';
 import AllItemsIcon from '../../svg/allItems.svg';
 import ArrowIcon from '../../svg/arrow.svg';
 import BellIcon from '../../svg/bell.svg';
 import BinocularsIcon from '../../svg/binoculars.svg';
 import BoardIcon from '../../svg/board.svg';
+import BoldIcon from '../../svg/bold.svg';
 import CameraIcon from '../../svg/camera.svg';
 import CheckIcon from '../../svg/check.svg';
 import ChevronLeftIcon from '../../svg/chevronLeft.svg';
 import ClipboardIcon from '../../svg/clipboard.svg';
 import CloudUploadIcon from '../../svg/cloudUpload.svg';
+import CodeIcon from '../../svg/code.svg';
+import CodeBlockIcon from '../../svg/codeBlock.svg';
 import CommentIcon from '../../svg/comment.svg';
 import CopyIcon from '../../svg/copy.svg';
 import CreateBoardIcon from '../../svg/createBoard.svg';
@@ -26,34 +32,47 @@ import ErrorIcon from '../../svg/error.svg';
 import ExpireTimeIcon from '../../svg/expireTime.svg';
 import FolderIcon from '../../svg/folder.svg';
 import GearIcon from '../../svg/gear.svg';
+import HeadlineFirstIcon from '../../svg/headlineFirst.svg';
+import HeadlineSecondIcon from '../../svg/headlineSecond.svg';
 import ImageIcon from '../../svg/image.svg';
 import InfoIcon from '../../svg/info.svg';
+import ItalicIcon from '../../svg/italic.svg';
 import KeyIcon from '../../svg/key.svg';
 import LogoutIcon from '../../svg/logout.svg';
 import MailIcon from '../../svg/mail.svg';
 import MoveIcon from '../../svg/move.svg';
 import NewWindowIcon from '../../svg/newWindow.svg';
+import NibIcon from '../../svg/nib.svg';
 import NotesIcon from '../../svg/notes.svg';
 import NotesBoldIcon from '../../svg/notesBold.svg';
+import OrderedListIcon from '../../svg/orderedList.svg';
 import OthersIcon from '../../svg/others.svg';
 import PadlockLockIcon from '../../svg/padlockLock.svg';
 import PadlockUnlockIcon from '../../svg/padlockUnlock.svg';
+import ParagraphIcon from '../../svg/paragraph.svg';
 import PenIcon from '../../svg/pen.svg';
+import PhotoIcon from '../../svg/photo.svg';
 import PrivateIcon from '../../svg/private.svg';
 import ProfileIcon from '../../svg/profile.svg';
 import PublicIcon from '../../svg/public.svg';
 import PublicFolderIcon from '../../svg/publicFolder.svg';
+import QuestionMarkIcon from '../../svg/questionMark.svg';
+import QuoteIcon from '../../svg/quote.svg';
 import RemoveTagIcon from '../../svg/removeTag.svg';
 import SearchIcon from '../../svg/search.svg';
 import SearchBoldIcon from '../../svg/searchBold.svg';
 import SendIcon from '../../svg/send.svg';
+import SeparatorIcon from '../../svg/separator.svg';
 import ShareIcon from '../../svg/share.svg';
 import SharedIcon from '../../svg/shared.svg';
 import StarIcon from '../../svg/star.svg';
 import TagFilledIcon from '../../svg/tagFilled.svg';
 import TagsIcon from '../../svg/tags.svg';
+import TaskIcon from '../../svg/task.svg';
 import TeamIcon from '../../svg/team.svg';
 import TrashBinIcon from '../../svg/trashBin.svg';
+import UnderlineIcon from '../../svg/underline.svg';
+import UnorderedListIcon from '../../svg/unorderedList.svg';
 import UpIcon from '../../svg/up.svg';
 import UploadIcon from '../../svg/upload.svg';
 import UrlIcon from '../../svg/url.svg';
@@ -70,16 +89,22 @@ import ZoomResetIcon from '../../svg/zoomReset.svg';
 export {
   AddPeopleIcon,
   AddToBoardIcon,
+  AlignCenterIcon,
+  AlignLeftIcon,
+  AlignRightIcon,
   AllItemsIcon,
   ArrowIcon,
   BellIcon,
   BinocularsIcon,
   BoardIcon,
+  BoldIcon,
   CameraIcon,
   CheckIcon,
   ChevronLeftIcon,
   ClipboardIcon,
   CloudUploadIcon,
+  CodeIcon,
+  CodeBlockIcon,
   CommentIcon,
   CopyIcon,
   CreateBoardIcon,
@@ -96,34 +121,47 @@ export {
   ExpireTimeIcon,
   FolderIcon,
   GearIcon,
+  HeadlineFirstIcon,
+  HeadlineSecondIcon,
   ImageIcon,
   InfoIcon,
+  ItalicIcon,
   KeyIcon,
   LogoutIcon,
   MailIcon,
   MoveIcon,
   NewWindowIcon,
+  NibIcon,
   NotesIcon,
   NotesBoldIcon,
+  OrderedListIcon,
   OthersIcon,
   PadlockLockIcon,
   PadlockUnlockIcon,
+  ParagraphIcon,
   PenIcon,
+  PhotoIcon,
   PrivateIcon,
   ProfileIcon,
   PublicIcon,
   PublicFolderIcon,
+  QuestionMarkIcon,
+  QuoteIcon,
   RemoveTagIcon,
   SearchIcon,
   SearchBoldIcon,
   SendIcon,
+  SeparatorIcon,
   ShareIcon,
   SharedIcon,
   StarIcon,
   TagFilledIcon,
   TagsIcon,
+  TaskIcon,
   TeamIcon,
   TrashBinIcon,
+  UnderlineIcon,
+  UnorderedListIcon,
   UpIcon,
   UploadIcon,
   UrlIcon,
